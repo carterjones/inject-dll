71dc86eb821cf2f8191207ab13cb303c6591387c
Carter Jones <carter@carterjones.info>
2014-02-25 13:20:15 -0800

    first post!

---
+++ InjectDLL/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace InjectDLL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Nouzuru;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DllInjector di = new DllInjector();
+            di.Open("calc");
+            string dllPath = @"..\hello-world-dll\hello-world-dll\x86-64\hello-world.dll";
+            di.InjectDll(dllPath);
+            Console.ReadKey();
+        }
+    }
+}

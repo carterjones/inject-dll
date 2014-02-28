e043f6587ec88aa0606f977dc25feaedd9c8725d
Carter Jones <carter@carterjones.info>
2014-02-28 14:03:34 -0800

    use command line arguments

--- InjectDLL/Program.cs
+++ InjectDLL/Program.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using Nouzuru;
@@ -11,10 +12,38 @@
         static void Main(string[] args)
         {
             DllInjector di = new DllInjector();
-            di.Open("calc");
-            string dllPath = @"..\hello-world-dll\hello-world-dll\x86-64\hello-world.dll";
-            di.InjectDll(dllPath);
-            Console.ReadKey();
+
+            if (args.Length != 2)
+            {
+                Console.WriteLine(
+                    "usage: " + System.AppDomain.CurrentDomain.FriendlyName +
+                    " <process_image_name> <absolute_dll_path>");
+                return;
+            }
+
+            bool passedTests = true;
+            string procName = args[0];
+            string dllPath = args[1];
+
+            if (!di.Open(procName))
+            {
+                Console.WriteLine(
+                    procName +
+                    " could not be opened. Verify that it is currently running and that you have the correct " +
+                    "permissions to open the process.");
+                passedTests = false;
+            }
+
+            if (!File.Exists(dllPath))
+            {
+                Console.WriteLine("The supplied DLL path does not exist.");
+                passedTests = false;
+            }
+
+            if (passedTests)
+            {
+                di.InjectDll(dllPath);
+            }
         }
     }
 }
